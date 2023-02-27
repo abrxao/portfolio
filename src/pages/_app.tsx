c1bff78f0f443d5361f0ac308e0fbf40f4a53fab
Abraão Albuquerque <abraao.hs2@gmail.com>
2023-02-27 17:44:37 -0300

    :package: build: adding webpack bundler analyzer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Abraão´s Portfolio</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,6 @@ export default function Document() {
           name="description"
           content="Um desenvolvedor web apaixonado por tecnologia e inovação, que está sempre em busca de novas formas de criar soluções digitais excepcionais. Descubra como ele pode ajudar a levar seus projetos online para o próximo nível."
         />
-        <title>Abraão´s Portfolio</title>
         <link
           rel="shortcut icon"
           type="image/x-icon"
