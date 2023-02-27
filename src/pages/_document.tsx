a93d1b13ab769df58b0db5324ff530c537e58354
Abraão Albuquerque <abraao.hs2@gmail.com>
2023-02-27 17:24:06 -0300

    adding title, favicon and compressing images

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,11 +3,23 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="description"
+          content="Um desenvolvedor web apaixonado por tecnologia e inovação, que está sempre em busca de novas formas de criar soluções digitais excepcionais. Descubra como ele pode ajudar a levar seus projetos online para o próximo nível."
+        />
+        <title>Abraão´s Portfolio</title>
+        <link
+          rel="shortcut icon"
+          type="image/x-icon"
+          href="/favicon.ico"
+        />
+      </Head>
+
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
