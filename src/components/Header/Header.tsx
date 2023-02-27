afc679ef93ba699b74df02c8c3824b06b341bb68
Abraão Albuquerque <abraao.hs2@gmail.com>
2023-02-27 16:57:28 -0300

    :zap: perfo: changing images extesion to webp

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,22 @@
-
+import { Tooltip } from "@material-tailwind/react";
+import Link from "next/link";
 import React, { FunctionComponent } from "react";
 
 const Header: FunctionComponent = () => {
   return (
     <header>
       <ul
-        className="py-7 w-full flex justify-center align-center gap-[1.75em] font-aero  text-orange tracking-wider absolute z-50
+        className="py-7 w-full flex justify-center align-center gap-[1.75em] font-aero  text-orange tracking-wider absolute z-[9999]
 
         2xl:text-5xl 
         xl:text-3xl
         lg:text-2xl
       "
       >
-        <li>about.</li>
-        <li>projects.</li>
+        <li>
+          <Link href="#about">about.</Link>
+        </li>
+        <li className="cursor-not-allowed"><Tooltip content="no ready">projects.</Tooltip></li>
         <li>contact.</li>
       </ul>
     </header>
--- src/components/SlideMenu/SlideMenu.tsx
+++ src/components/SlideMenu/SlideMenu.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useEffect, useState, useRef } from "react";
+import React, { FunctionComponent} from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper";
 import {
